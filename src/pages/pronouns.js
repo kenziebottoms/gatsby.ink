3d908d0843bcace4e2de6bbd16bb35e0df00c327
Kenzie Bottoms <kenziebottoms@gmail.com>
2021-08-05 01:19:57 -0500

    pronouns page

---
+++ src/pages/pronouns.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import Page from '../components/Page'
+
+const Pronouns = () => (
+  <Page className='blog'>
+    <div className='block'>
+      <h1>What does &ldquo;they/them&rdquo; mean?</h1>
+      <p>
+        In the context of blue being male and pink being female (which is
+        itself a very recent gender convention), I like to describe myself
+        as green. It&rsquo;s a little closer to blue than pink, but neither
+        end of the spectrum feels wholly right and neither set of conventional
+        pronouns fits perfectly.
+      </p>
+      <blockquote>
+        I&rsquo;m asking you to use the pronouns &ldquo;they,&rdquo;
+        &ldquo;them,&rdquo; and &ldquo;theirs&rdquo; when you talk about me.
+      </blockquote>
+      <p>For example:</p>
+      <ul>
+        <li>
+          &ldquo;<strong>They</strong> are at work right now.&rdquo;
+        </li>
+        <li>
+          &ldquo;You can ask <strong>them</strong> if you have questions.&rdquo;
+        </li>
+        <li>
+          &ldquo;<strong>Their</strong> hair is naturally brown.&rdquo;
+        </li>
+        <li>
+          &ldquo;That idea was <strong>theirs</strong>.&rdquo;
+        </li>
+        <li>
+          &ldquo;They are too lazy to make <strong>themself</strong> dinner.&rdquo;
+        </li>
+      </ul>
+      <p>
+        It&rsquo;s not a big deal if you mess up; you&rsquo;re not the only
+        one. Correct yourself and move on. If I correct you, that means you
+        have made me feel safe enough to advocate for myself. Believe it or
+        not, it&rsquo;s a compliment.
+      </p>
+    </div>
+  </Page>
+)
+
+export default Pronouns
